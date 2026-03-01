7a66bc564a1c0cac40381a8db02991a57dcbf19c
oluwaniyi adeloye <stephenniyiadeloye@gmail.com>
2026-03-01 11:03:04 +0100

    little-lemon

---
+++ src/NavBar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+        <div className="navbar-logo">
+            <img src="/assets/logo.png" alt="Little Lemon Logo" />
+        </div>
+        <ul className="navbar-links">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#menu">Menu</a></li>
+            <li><a href="#reservations">Reservations</a></li>
+            <li><a href="#order">Order Online</a></li>
+            <li><a href="#login/signUp">Login/Sign Up</a></li>
+            <li><a href="#about">About</a></li>
+        </ul>
+    </nav>
+  );
+}
